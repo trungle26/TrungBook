11d22ac66800ce08c870dd63eabf33657eb63d96
trungle26 <leductrung8520@gmail.com>
2023-08-05 11:04:10 +0700

    first commit

---
+++ TrungBook.WebApi/TrungBook.WebApi/Controllers/TrungBookController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TrungBook.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class TrungBookController : ControllerBase
+    {
+        public TrungBookController()
+        {
+        }
+    }
+}
